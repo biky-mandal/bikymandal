cde62732e05db2a50c9c56d5328e37d363bb70f4
biky-mandal <m.biky50@gmail.com>
2024-02-21 21:41:55 +0530

    Image path updated

--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -12,8 +12,8 @@ export class HomeComponent {
       period: 'Feb 2022 - Apr 2022',
       stack: ['html', 'css', 'js', 'react'],
       weblink: 'https://crewbella.com/',
-      profile: '../../../assets/images/crewbella.jpg',
-      cert: '../../../assets/pdfs/crewbella-certificate.pdf',
+      profile: 'assets/images/crewbella.jpg',
+      cert: 'assets/pdfs/crewbella-certificate.pdf',
       class: 'oecard crebella'
     },
     {
@@ -21,21 +21,21 @@ export class HomeComponent {
       period: 'May 2021 - Aug 2021',
       stack: ['html', 'css', 'js', 'react'],
       weblink: 'https://mental-sphere-web-ten.vercel.app/',
-      profile: '../../../assets/images/mindtree.jpg',
-      cert: '../../../assets/pdfs/ms-certificate.pdf',
+      profile: 'assets/images/mindtree.jpg',
+      cert: 'assets/pdfs/ms-certificate.pdf',
       class: 'oecard ms'
     }
   ]
 
   projectCard: any = [
     {
-      icon: '../../../assets/images/algorithm-96.png',
+      icon: 'assets/images/algorithm-96.png',
       title: 'Algorithm Visualizer',
       link: 'https://visualizer-v2.web.app/',
       git: 'https://github.com/biky-mandal/youtubeclone'
     },
     {
-      icon: '../../../assets/images/youtube-240.png',
+      icon: 'assets/images/youtube-240.png',
       title: 'Youtube Clone',
       link: 'https://biky-mandal.github.io/youtubeclone/',
       git: 'https://github.com/biky-mandal/youtubeclone'
