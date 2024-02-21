24a81b5f6691d789a30b47befe97516558f4f146
biky-mandal <m.biky50@gmail.com>
2024-02-21 23:30:44 +0530

    welcome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,8 +15,8 @@ export class AppComponent implements OnInit{
   ){}
 
   ngOnInit(): void {
-    if(!sessionStorage.getItem('visit')){
-      this.router.navigate(['/welcome'])
-    }
+    // if(!sessionStorage.getItem('visit')){
+    //   this.router.navigate(['/welcome'])
+    // }
   }
 }
